73ee9e32b329f318fd7c669cdd3e74dd8a0f0d4a
Lup1n-3 <luckg3030@gmail.com3>
2024-07-25 23:28:51 -0300

    test

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, render_template
 import subprocess
 import os
-import threading
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
@@ -16,22 +15,14 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 # Variables para manejar procesos de hashcat y salida en tiempo real
 hashcat_process = None
-output_lines = []
-
-def capture_output(process):
-    global output_lines
-    for line in iter(process.stdout.readline, ''):
-        output_lines.append(line)
-        if process.poll() is not None:
-            break
 
 @app.route('/')
 def index():
-    return render_template('index.html', output=''.join(output_lines))
+    return render_template('index.html', output='')
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    global hashcat_process, output_lines
+    global hashcat_process
 
     if hashcat_process and hashcat_process.poll() is None:
         return render_template('index.html', output="Hashcat is already running. Please stop it before starting a new analysis.")
@@ -65,20 +56,23 @@ def submit():
     try:
         xterm_command = f'xterm -hold -e "{command}"'
         hashcat_process = subprocess.Popen(xterm_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        output_lines = []
-        output_thread = threading.Thread(target=capture_output, args=(hashcat_process,))
-        output_thread.start()
+        output = ''
+        for line in iter(hashcat_process.stdout.readline, b''):
+            output += line.decode('utf-8')
     except Exception as e:
-        output_lines.append(f"Error: {str(e)}")
+        output = f"Error: {str(e)}"
 
-    return render_template('index.html', output=''.join(output_lines))
+    # Imprimir salida para depuración
+    print(f"Salida: {output}")
+
+    return render_template('index.html', output=output)
 
 @app.route('/update', methods=['POST'])
 def update():
     global hashcat_process
     if hashcat_process and hashcat_process.poll() is None:
         try:
-            hashcat_process.stdin.write('s\n')
+            hashcat_process.stdin.write(b's\n')
             hashcat_process.stdin.flush()
             return render_template('index.html', output="Sent 's' to hashcat.")
         except Exception as e:
@@ -90,7 +84,7 @@ def quit():
     global hashcat_process
     if hashcat_process and hashcat_process.poll() is None:
         try:
-            hashcat_process.stdin.write('q\n')
+            hashcat_process.stdin.write(b'q\n')
             hashcat_process.stdin.flush()
             hashcat_process.terminate()  # Terminate the process
             hashcat_process = None
